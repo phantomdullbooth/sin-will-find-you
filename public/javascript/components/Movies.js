53c6a1d4de907993d3f23895831a87f2dbbe7f5e
A Winslow Mays <49768250+winslowdev@users.noreply.github.com>
2019-09-18 01:25:14 -0400

    committing css because no apikey in it

--- public/javascript/components/Movies.js
+++ public/javascript/components/Movies.js
@@ -5,23 +5,23 @@ class Movies extends React.Component {
         console.log(this.props.movie)
         return (
             <div className="media">
-                {this.props.movie.map((movies, ross) => {
+                {this.props.homeMovies.map((homeMovie, index) => {
                     return (
-                        <div className="media-card-movie" key={ross}>
+                        <div className="media-card-movie" key={index}>
 
-                            {(movies.poster_path 
-                                ? <img className="movie" src={"https://image.tmdb.org/t/p/w300/" + movies.poster_path} />
+                            {(homeMovie.poster_path 
+                                ? <img className="movie" src={"https://image.tmdb.org/t/p/w300/" + homeMovie.poster_path} />
                                 : <div className="movie-null">
                                     <h3>Image unavailable</h3>
                                 </div>
                                 )}
 
-                            {( movies.name
-                                ? <h5><span className="important">{movies.name}</span></h5>
-                                : <h5><span className="important">{movies.title}</span></h5>
+                            {( homeMovie.name
+                                ? <h5><span className="important">{homeMovie.name}</span></h5>
+                                : <h5><span className="important">{homeMovie.title}</span></h5>
                                 )}
 
-                                <p>{movies.media_type}</p>
+                                <p>{homeMovie.media_type}</p>
                         </div>
                     )
                 })}
