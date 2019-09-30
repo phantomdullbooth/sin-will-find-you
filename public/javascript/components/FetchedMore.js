3df6e5737cd39733739e516bb854cc45a8b7b2ab
winslowdev <49768250+winslowdev@users.noreply.github.com>
2019-09-30 11:39:23 -0400

    restyling more results, youtube

--- public/javascript/components/FetchedMore.js
+++ public/javascript/components/FetchedMore.js
@@ -18,51 +18,49 @@ class FetchedMore extends React.Component {
                         </div>
                     )
                 })}
-                
+
                 {/* ======================= SERIES & MOVIES ======================= */}
 
                 {this.props.moreSeries.map((moreSeries, index) => {
                     return (
                         <div className="more-card" key={index}>
-                            <a  href={'https://www.themoviedb.org/movie/' + moreSeries.id} target="_blank">
-                            
+                            <a href={'https://www.themoviedb.org/movie/' + moreSeries.id} target="_blank">
+
                                 {/* IF POSTER, SHOW POSTED / OTHERWISE, SHOW REPLACEMENT */}
                                 {(moreSeries.poster_path
                                     ? <img className="series-more" src={"https://image.tmdb.org/t/p/w300/" + moreSeries.poster_path} />
                                     : <div className="series-null"><h3>Image unavailable</h3></div>
                                 )}
-                                </a>
+                            </a>
 
-                                {/* IF NAME, SHOW NAME / OTHERWISE, SHOW ORIGINAL TITLE */}
-                                {(moreSeries.name)
-                                    ? <h5><span>{moreSeries.name}</span></h5>
-                                    : <h5><span>{moreSeries.title}</span></h5>
-                                }
+                            {/* IF NAME, SHOW NAME / OTHERWISE, SHOW ORIGINAL TITLE */}
+                            {(moreSeries.name)
+                                ? <h5><span>{moreSeries.name}</span></h5>
+                                : <h5><span>{moreSeries.title}</span></h5>
+                            }
                         </div>
-                                )
-                            })}
-
-                    {/* ======================= YOUTUBE ======================= */}
-
-                    {this.props.moreYoutubes.map((moreYoutube, index) => {
-                        return (
-                            <div className="more-card" key={index}>
-                                <a  href={'https://www.youtube.com/watch?v=' + moreYoutube.videoId}
-                                    target="_blank">
-                                        <img className="youtube" src={moreYoutube.snippet.thumbnails.medium.url} />
-                                </a>
-                                
-                                <h5>
-                                    <span>{moreYoutube.snippet.channelTitle}</span>
-                                    <br />{moreYoutube.snippet.title}</h5>
-                            </div>
-                                    
-                                ) 
-                            })}
-                            
-                        </div>
-                        
                     )
-                    
-                }             
+                })}
+
+                {/* ======================= YOUTUBE ======================= */}
+
+                {this.props.moreYoutubes.map((moreYoutube, index) => {
+                    return (
+                        <div className="more-card" key={index}>
+                            <a href={'https://www.youtube.com/watch?v=' + moreYoutube.videoId}
+                                target="_blank">
+                                <img className="youtube" src={moreYoutube.snippet.thumbnails.medium.url} />
+                            </a>
+                            <h5>{moreYoutube.snippet.channelTitle}</h5>
+                            <p>{moreYoutube.snippet.title}</p>
+                        </div>
+
+                    )
+                })}
+
+            </div>
+
+        )
+
+    }
 }
