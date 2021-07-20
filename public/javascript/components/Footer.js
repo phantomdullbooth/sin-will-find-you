250c2fb285b86c80fc6554d2440469730d21069d
Addison <49768250+winslowdev@users.noreply.github.com>
2021-07-19 20:56:01 -0400

    Updated link 'pon footer

--- public/javascript/components/Footer.js
+++ public/javascript/components/Footer.js
@@ -1,13 +1,8 @@
-
-// ============================================ # FOOTER //
-// ============================================ # FOOTER //
-
-
 class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <p><a href="https://wins.dev" target="_blank">winslow</a> enjoys short walks to the couch, chamomile tea, and true crime novels.</p>
+                <p><a href="https://github.com/winslowdev" target="_blank">winslow</a> enjoys short walks to the couch, chamomile tea, and true crime novels.</p>
             </footer>
         )
     }
